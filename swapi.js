588fc4d8cfe2bf317dd822a17119ff000cc0de30
NathanB09 <nathanbell09@gmail.com>
2019-01-10 16:24:55 +0000

    Done.

--- swapi.js
+++ swapi.js
@@ -1,1 +1,18 @@
 // Write your swapi code in this file!
+let promiseForData = fetch('https://swapi.co/api/films/1/')
+const crawlBtn = document.querySelector('#crawlBtn')
+const crawlDiv = document.querySelector('#crawlDiv')
+
+function getOpeningCrawl() {
+  promiseForData
+    .then(res => res.json())
+    .then(json => {
+      crawlDiv.innerHTML = json.opening_crawl
+    })
+}
+
+crawlBtn.addEventListener('click', getOpeningCrawl)
+
+function getPlanet() {
+  
+}
