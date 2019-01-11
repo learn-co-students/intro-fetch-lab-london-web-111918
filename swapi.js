05438463556a8f28302868f91462dada838527cb
morwen80 <ribesnero80@gmail.com>
2019-01-11 13:22:24 +0000

    Done.

--- swapi.js
+++ swapi.js
@@ -7,30 +7,30 @@ function getOpeningCrawl(){
   .then(json => console.log(json.opening_crawl));
 }
 
+
 const planetUserInput = document.getElementById("planetInput");
+const renderPlanet = document.getElementById("planetData");
 
 function getPlanet(){
 fetch(`https://swapi.co/api/planets/${planetUserInput.value}/`)
 .then(res => res.json())
-.then(data => {
-      console.log(data)
-      // for each pokemon
-      // - DONE create a element
-      // - DONE fill it with h1 and image
-      // - render that pokemon to the page
-      data.forEach(renderPlanet)
-  })
-}
-
-const renderPlanet = (planet) => {
-  const saidPlanet = document.createElement('div')
-  said.innerHTML = `
-    <h2>${planet.name}</h2>
-    <br>
-    <b>Climate:</b> ${planet.climate.value}
-  `
-  saidPlanet.appendChild(renderPlanet.value)
+.then(planet => renderPlanet.innerHTML = `
+  <h2>${planet.name}
+  <br>
+  <b>Climate:</b> ${planet.climate}
+  </h2>`
+)
+  }
 
 
-
-}
+// const renderPlanet = (planet) => {
+//   const saidPlanet = document.createElement('div')
+//
+//    saidPlanet.innerHTML = `
+//     <h2>${planet.name}</h2>
+//     <br>
+//     <b>Climate:</b> ${planet.climate.value}
+//   `
+//   saidPlanet.appendChild(renderPlanet.value)
+//
+// }
