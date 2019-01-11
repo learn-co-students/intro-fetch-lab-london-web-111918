f8fd8f30a3bfdb9132776d55845d34fb9e5b5b1c
morwen80 <ribesnero80@gmail.com>
2019-01-11 15:18:59 +0000

    rendered droids on page

--- swapi.js
+++ swapi.js
@@ -17,7 +17,36 @@ fetch(`https://swapi.co/api/planets/${planetUserInput.value}/`)
 .then(planet => renderPlanet.innerHTML = `
   <h2>${planet.name}
   <br>
-  <b>Climate:</b> ${planet.climate}
+  Climate: ${planet.climate}
   </h2>`
 )
   }
+
+
+function getDroids(id){
+  const droidEl = document.querySelector("#droid-" + id);
+    fetch(`https://swapi.co/api/people/${id}/`)
+    .then(res => res.json())
+    .then(droid => {
+      droidEl.innerHTML = `${droid.name}
+      <br>${droid.mass}
+      <br>${droid.homeworld}`
+    })
+  }
+
+
+//  fetch data for droids
+// they're in https://swapi.co/api/people/ with id 2 and 3
+
+//  find each droid's name, height and mass in their own span
+//   click either of their button triggers getHomePlanet()
+//  call the API again
+//   find the corresponding homePlanet
+
+
+
+document.addEventListener('load', getDroids(2))
+
+
+const renderSingleDroid = (droid) => {
+}
