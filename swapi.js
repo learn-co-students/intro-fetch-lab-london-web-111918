ff5ef8ac6fb8e4a728e35e86e7aac9be30239c7f
morwen80 <ribesnero80@gmail.com>
2019-01-11 10:39:19 +0000

    Done.

--- swapi.js
+++ swapi.js
@@ -6,3 +6,31 @@ function getOpeningCrawl(){
   .then(res => res.json())
   .then(json => console.log(json.opening_crawl));
 }
+
+const planetUserInput = document.getElementById("planetInput");
+
+function getPlanet(){
+fetch(`https://swapi.co/api/planets/${planetUserInput.value}/`)
+.then(res => res.json())
+.then(data => {
+      console.log(data)
+      // for each pokemon
+      // - DONE create a element
+      // - DONE fill it with h1 and image
+      // - render that pokemon to the page
+      data.forEach(renderPlanet)
+  })
+}
+
+const renderPlanet = (planet) => {
+  const saidPlanet = document.createElement('div')
+  said.innerHTML = `
+    <h2>${planet.name}</h2>
+    <br>
+    <b>Climate:</b> ${planet.climate.value}
+  `
+  saidPlanet.appendChild(renderPlanet.value)
+
+
+
+}
