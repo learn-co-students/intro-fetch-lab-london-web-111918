47d2c2a88b37c796f6d5595be14307d172f10e2e
morwen80 <ribesnero80@gmail.com>
2019-01-12 13:09:23 +0000

    First Numbers exercise solved

--- numbers.js
+++ numbers.js
@@ -1,1 +1,8 @@
-// Write your numbers code in this file!
+// const numberAPI = "http://numbersapi.com/"
+const factButton = document.getElementById('one-facts')
+
+function numberOne(){
+fetch(`http://numbersapi.com/1/trivia`)
+.then(res => res.text()
+.then(fact => factButton.innerText = `${fact}`))
+}
