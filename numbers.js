83ff7e41d6525a55b355bb5271f794e6b6b206a5
Andy <andypurbrick@gmail.com>
2019-01-13 12:55:52 +0000

    Done.

--- numbers.js
+++ numbers.js
@@ -1,1 +1,67 @@
-// Write your numbers code in this file!
+//number one
+const numberOneBtn = document.querySelector('#number-one')
+const oneDiv = document.querySelector('#one-facts')
+
+function numberOne() {
+  fetch('http://numbersapi.com/1/trivia')
+    .then(res => res.text())
+    .then(text => {
+      oneDiv.innerHTML = text
+    })
+}
+
+numberOneBtn.addEventListener('click', numberOne)
+
+
+//pick number
+const numberInput = document.querySelector('#pick-a-number')
+const numberDiv = document.querySelector('#random-math-fact')
+
+ function pickNumber() {
+  if (!parseInt(numberInput.value)) {
+    numberDiv.innerHTML = "please enter a valid number"
+  } else {
+    fetch(`http://numbersapi.com/${numberInput.value}/trivia`)
+      .then(res => res.text())
+      .then(text => {
+        numberDiv.innerHTML = text
+      })
+  }
+}
+
+numberInput.addEventListener('input', pickNumber)
+
+//study history
+let year = new Date().getFullYear()
+const yearDiv = document.querySelector('#year-history')
+
+ function studyHistory() {
+  fetch(`http://numbersapi.com/${year}/year`)
+    .then(res => res.text())
+    .then(text => {
+      yearDiv.innerHTML = text
+    })
+}
+
+ setInterval(studyHistory, 5000)
+
+
+ // all the numbers
+ const numbersBtn = document.querySelector('#all-numbers-button')
+ const allNumbersDiv = document.querySelector('#all-the-numbers')
+ const numberList = document.createElement('ul')
+
+ function allNumbers() {
+   fetch('http://numbersapi.com/1..100')
+     .then(res => res.json())
+     .then(json => {
+       for (const key in json) {
+         let num = document.createElement('li')
+         num.innerHTML = json[key]
+         numberList.appendChild(num)
+       }
+       allNumbersDiv.appendChild(numberList)
+     })
+ }
+
+ numbersBtn.addEventListener('click', allNumbers)
--- swapi.js
+++ swapi.js
@@ -1,1 +1,61 @@
-// Write your swapi code in this file!
+//opening crawl
+
+let promiseForFilmData = fetch('https://swapi.co/api/films/1/')
+const crawlBtn = document.querySelector('#crawlBtn')
+const crawlDiv = document.querySelector('#crawlDiv')
+
+function getOpeningCrawl() {
+  promiseForFilmData
+    .then(res => res.json())
+    .then(json => {
+      crawlDiv.innerHTML = json.opening_crawl
+    })
+}
+
+crawlBtn.addEventListener('click', getOpeningCrawl)
+
+
+//planets
+
+const planetForm = document.querySelector('#planetInput')
+const planetDiv = document.querySelector('#planetData')
+const planetBtn = document.querySelector('#findPlanet')
+
+function getPlanet() {
+    fetch(`https://swapi.co/api/planets/${planetForm.value}/`)
+    .then(res => res.json())
+    .then(json => {
+      planetDiv.innerHTML = `${json.name}, ${json.climate}`
+    })
+}
+
+planetBtn.addEventListener('click', getPlanet)
+
+
+//droids
+
+const droidBtn2 = document.querySelector('#droid-2-btn')
+const droidBtn3 = document.querySelector('#droid-3-btn')
+
+
+function getDroids(id) {
+  const droidDiv = document.querySelector(`#droid-${id}`)
+  const droidPlanet = document.querySelector(`#droid-homeworld-${id}`)
+  let getHomePlanet
+  let promiseForDroids = fetch(`https://swapi.co/api/people/${id}/`)
+
+  promiseForDroids
+    .then(res => res.json())
+    .then(json => {
+      droidDiv.innerHTML = `Name: ${json.name}, Height: ${json.height}, Mass: ${json.mass}`
+      fetch(json.homeworld)
+        .then(res => res.json())
+        .then(json => {
+        droidPlanet.innerHTML = `Homeworld Name: ${json.name}, Climate: ${json.climate}`
+      })
+    })
+
+}
+
+droidBtn2.addEventListener('click', () => getDroids(2))
+droidBtn3.addEventListener('click', () => getDroids(3))
